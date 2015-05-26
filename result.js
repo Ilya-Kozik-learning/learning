a93f5ed4f901a6207b6ef531e5e15476cc7f95d1
Ilya-Kozik <Ilya.Kozik@itechart-group.com>
2015-05-27 01:53:55 +0300

    Problem 1.

---
+++ result.js
@@ -0,0 +1,19 @@
+﻿var practise1 = (
+	function () {
+		"use strict";
+
+		return {
+			F: function () {
+				var args = Array.prototype.slice.call(arguments);
+				var func = args.pop();
+
+				var resultFunction = function () {
+					var newArgs = Array.prototype.slice.call(arguments);
+
+					return func.apply(null, args.concat(newArgs));
+				};
+
+				return resultFunction;
+			}
+		}
+	}());
---
+++ testing.js
@@ -0,0 +1,7 @@
+﻿var sum = function (a, b, c) {
+	return a + b + c;
+};
+
+var result = practise1.F(practise1.F(1, sum))(4,4);
+
+alert(result);
