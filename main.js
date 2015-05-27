7ec0fd4a2ec001c3292a8ef273cb5b3cf3796dbd
Ilya-Kozik <Ilya.Kozik@itechart-group.com>
2015-05-28 00:08:15 +0300

    Problem 8: Sum of random numbers

--- main.js
+++ main.js
@@ -14,3 +14,21 @@ var sumFunction = function (prev, current) {
 
 practise1.fold(practise1.filter(arrayWithEven, isEven), sumFunction);
 console.log("Problem 7 result: " + sum / count);
+
+//Problem 8: Sum of random numbers
+var randomFunction = function (stateValue) {
+	if (stateValue >= 10) {
+		return false;
+	}
+
+	return {
+		element: Math.floor((Math.random() * 5) + 1),
+		stateValue: stateValue + 1
+	};
+}
+
+sum = 0;
+currentResult = practise1.fold(practise1.unfold(randomFunction, 0), sumFunction);
+console.log("Problem 8 result: " + sum);
+
+//
