e142f3fb5b4860ef48a9892499d9d3d3a745e424
Ilya-Kozik <Ilya.Kozik@itechart-group.com>
2015-05-27 23:39:57 +0300

    Problem 7: Average of even numbers

---
+++ main.js
@@ -0,0 +1,16 @@
+﻿//Problem 7: Average of even numbers
+var isEven = function (value) {
+	return value % 2 == 0;
+}
+var arrayWithEven = [1, 2, 3, 4, 6, 8];
+
+var sum = 0;
+var count = 0;
+
+var sumFunction = function (prev, current) {
+	sum += current;
+	++count;
+}
+
+practise1.fold(practise1.filter(arrayWithEven, isEven), sumFunction);
+console.log("Problem 7 result: " + sum / count);
--- testing.js
+++
@@ -1,7 +0,0 @@
-﻿var testFunction = function (value) {
-	return value % 2 == 0;
-}
-var testArray = [1, 2, 3, 4, 6, 8];
-
-console.log(testArray);
-console.log(practise1.filter(testArray, testFunction));
