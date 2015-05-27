ed55f33b0920668f6eeecec08fe24339f27a0a93
Ilya-Kozik <Ilya.Kozik@itechart-group.com>
2015-05-27 03:04:22 +0300

    Problem 2. Currying

--- result.js
+++ result.js
@@ -2,6 +2,36 @@
 	function () {
 		"use strict";
 
+		var _curry = function (func, args) {
+			if (func.length <= 1) {
+				return func;
+			}
+
+			if (args) {
+				if (func.length - 1 == args.length) {
+					return function (last) {
+						args.push(last);
+
+						return func.apply(undefined, args);
+					};
+				}
+				else {
+					return function (arg) {
+						args.push(arg);
+
+						return _curry(func, args);
+					}
+				}
+			}
+			else {
+				return function (arg) {
+					args = [arg];
+
+					return _curry(func, args);
+				}
+			}
+		};
+
 		return {
 			F: function () {
 				var args = Array.prototype.slice.call(arguments);
@@ -14,6 +44,9 @@
 				};
 
 				return resultFunction;
+			},
+			curry: function (func) {
+				return _curry(func);
 			}
 		}
 	}());
--- testing.js
+++ testing.js
@@ -1,7 +1,40 @@
-﻿var sum = function (a, b, c) {
-	return a + b + c;
+﻿var sum = function () {
+	return "test";
 };
 
-var result = practise1.F(practise1.F(1, sum))(4,4);
+var sum1 = function (a) {
+	return a;
+}
+
+var sum2 = function (a, b) {
+	return a + b;
+}
+
+var sum3 = function (a, b, c) {
+	return a + b + c;
+}
+
+var sum4 = function (a, b, c, d) {
+	return a + b + c + d;
+}
+
+
+var result = practise1.curry(sum)();
+
+alert(result);
+
+result = practise1.curry(sum1)("one parameter test");
+
+alert(result);
+
+result = practise1.curry(sum2)(" 1 ")(" 2 ");
+
+alert(result);
+
+result = practise1.curry(sum3)(" 1 ")(" 2 ")(" 3 ");
+
+alert(result);
+
+result = practise1.curry(sum4)(" 1 ")(" 2 ")(" 3 ")(" 4 ");
 
 alert(result);
