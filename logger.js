24d4bb752e9cf1c5e36e459cc5277e89f8746476
Ilya.Kozik <Ilya.Kozik@itechart-group.com>
2015-06-10 19:44:49 +0100

    Logger basic functionality.

---
+++ logger.js
@@ -0,0 +1,73 @@
+﻿define(function () {
+	'use strict'
+
+	function Logger(writeInfoMethod, writeErrorMethod) {
+		alert("constructor");
+
+		///log methods
+		function _logInfo(writeMethod, message) {
+			writeMethod("Info:" + message);
+		};
+
+		function _logError(writeMethod, message) {
+			writeMethod("Error:" + message);
+		};
+		///
+
+		///writers
+		function _writeToConsole(message) {
+			console.log(message);
+		};
+
+		function _writeToAlert(message) {
+			alert(message);
+		};
+
+		function _writeToCurrentWindow(message) {
+			var body = window.document.getElementsByTagName("body")[0];
+			body.innerHTML += "\n" + message;
+		}
+		///
+
+		///check
+		if (writeInfoMethod) {
+			if (typeof writeInfoMethod != 'function' || writeInfoMethod.length < 1) {
+				throw new TypeError("writeInfoMethod should be function(string)");
+			}
+		}
+		else {
+			writeInfoMethod = _writeToConsole;
+		}
+
+		if (writeErrorMethod) {
+			if (typeof writeErrorMethod != 'function' || writeErrorMethod.length < 1) {
+				throw new TypeError("writeErrorMethod should be function(string)");
+			}
+		}
+		else {
+			writeErrorMethod = _writeToConsole;
+		}
+		///
+
+		var self = {
+			///writers
+			writeToConsole: _writeToConsole,
+			writeToAlert: _writeToAlert,
+			writeToCurrentWindow: _writeToCurrentWindow,
+			///
+
+			///log methods
+			logInfo: _logInfo.bind(this, writeInfoMethod),
+			logError: _logError.bind(this, writeErrorMethod)
+			///
+		}
+
+		self.constructor = Logger;
+
+		return self;
+	}
+
+	return {
+		Logger: Logger
+	};
+});
---
+++ main.js
@@ -0,0 +1,10 @@
+﻿define(function (require) {
+	var logger = require('logger').Logger(null, null);
+
+	logger.writeToConsole("test console");
+	logger.writeToAlert("test alert");
+	logger.writeToCurrentWindow("test current window");
+
+	logger.logInfo("test info");
+	logger.logError("test error")
+});
